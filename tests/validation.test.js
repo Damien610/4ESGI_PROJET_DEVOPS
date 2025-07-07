341e3e0db7943f65b833018bd57cc574c7c96421
Damien le padawan du coding <nerridamien@gmail.com>
2025-07-07 16:18:13 +0200

    fix end

--- tests/validation.test.js
+++ tests/validation.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../app');
+const pool = require('../db');
 
 describe('Validation des tickets', () => {
     test('Refuse un ticket sans email', async () => {
@@ -19,4 +20,8 @@ describe('Validation des tickets', () => {
 
         expect(res.statusCode).toBe(400);
     });
+});
+
+afterAll(async () => {
+    await pool.end();
 });
