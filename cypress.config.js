c5cd70bfcb10edf0343359d76971482318314bb1
Damien610 <nerridamien@gmail.com>
2025-07-11 18:04:09 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,9 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    supportFile: false,
     setupNodeEvents(on, config) {
-      supportFile: false,
+      
     },
   },
 });
